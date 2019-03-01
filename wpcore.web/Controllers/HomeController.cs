59c83b9c056fa419e3ca0c5da0a941c4ed0afd93
Ali Suleyman TOPUZ <alisuleyman.topuz@alten.se>
2019-03-01 16:50:28 +0100

    Add project files.

---
+++ wpcore.web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace AspNetWebpack.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
